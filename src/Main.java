fe9d57b658349b9ade2892451731e5f9249c5f0e
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2019-03-20 23:09:04 -0600

    Testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException, IOException {
+
+        System.out.println(new File(".").getAbsoluteFile());
+
+        FileReader reader = new FileReader("TestCode.txt");
+        BufferedReader buffer = new BufferedReader(reader);
+        StreamTokenizer token = new StreamTokenizer(buffer);
+
+        boolean arg = true;
+        token.slashStarComments(true);
+
+        int t;
+        while ((t = token.nextToken()) != StreamTokenizer.TT_EOF)
+        {
+            switch (t)
+            {
+                case StreamTokenizer.TT_WORD:
+                    System.out.println("Word : " + token.sval);
+                    break;
+                case StreamTokenizer.TT_NUMBER:
+                    System.out.println("Number : " + token.nval);
+                    break;
+            }
+        }
     }
 }
