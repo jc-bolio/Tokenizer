b93bebd106932c7f9d01624100f64e93a5c052db
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2019-03-20 23:21:27 -0600

    Print line no and id and num

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException, FileNotFoundException, IOException {
 
-        System.out.println(new File(".").getAbsoluteFile());
-
         FileReader reader = new FileReader("TestCode.txt");
         BufferedReader buffer = new BufferedReader(reader);
         StreamTokenizer token = new StreamTokenizer(buffer);
 
-        boolean arg = true;
-        token.slashStarComments(true);
+        boolean ignorarComentarios = true;
+        token.slashStarComments(ignorarComentarios);
+        token.slashSlashComments(ignorarComentarios);
 
         int t;
         while ((t = token.nextToken()) != StreamTokenizer.TT_EOF)
@@ -19,10 +18,11 @@ public class Main {
             switch (t)
             {
                 case StreamTokenizer.TT_WORD:
-                    System.out.println("Word : " + token.sval);
+
+                    System.out.println("Linea " + token.lineno() + ": id : " + token.sval);
                     break;
                 case StreamTokenizer.TT_NUMBER:
-                    System.out.println("Number : " + token.nval);
+                    System.out.println("Linea " + token.lineno() + ": núm : " + token.nval);
                     break;
             }
         }
